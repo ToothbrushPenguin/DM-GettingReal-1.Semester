32515de757164ef9e5c74fddd6f0e8b45fabed6c
ToothbrushPenguin <emil.k.m@hotmail.dk>
2024-12-12 13:18:19 +0100

    update add merge

--- Code/KvalitetesLedelsesSystem/KvalitetesLedelsesSystem/ViewModels/UserViewModel.cs
+++ Code/KvalitetesLedelsesSystem/KvalitetesLedelsesSystem/ViewModels/UserViewModel.cs
@@ -1,61 +1,4 @@
-<<<<<<< HEAD
-﻿using KvalitetesLedelsesSystem.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KvalitetesLedelsesSystem.ViewModels
-{
-    public class UserViewModel : BaseViewModel
-    {
-        private User user;
-        public string UserName { get; set; }
-        public string Name { get; set; }
-        public string Company { get; set; }
-        public string Password { get; set; }
-
-        public UserViewModel(User user) 
-        {
-            this.user = user;
-            UserName = user.UserName;
-            Name = user.Name;
-            Company = user.Company;
-
-
-
-            if (user is Admin a)
-            {
-                Password = a.Password;
-            }
-            else if (user is Contingency_Responsible c)
-            {
-                Password = c.Password;
-            }
-            else
-            {
-                Password = "";
-            }
-        }
-
-        public void Delete(UserRepository userRepository)
-        {
-            userRepository.Remove(user.UserName);
-        }
-
-        public void ChangeCheck(UserRepository userRepository, string username)
-        {
-            userRepository.ChangeCheck(username);
-            userRepository.UpdateLog(username);
-        }
-        
-
-
-    }
-}
-=======
-﻿using KvalitetesLedelsesSystem.Models;
+using KvalitetesLedelsesSystem.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -296,4 +239,3 @@ namespace KvalitetesLedelsesSystem.ViewModels
         }
     }
 }
->>>>>>> main
